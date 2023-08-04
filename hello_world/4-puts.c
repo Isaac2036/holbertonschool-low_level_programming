44241dec4c1e4283c35cc094d8d064c47ecb7e5b
isaac2036 <isaacpablo0596@hotmail.com>
2023-08-03 23:59:33 -0700

    Hello, puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 /**
  * main - print "Programming is like building a multilingual puzzle"
  *
@@ -5,6 +6,6 @@
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("Programming is like building a multilingual puzzle");
 	return (0);
 }
