513c4f1fffdae97e89ce703eb45ffc1eea4c456e
isaac2036 <isaacpablo0596@hotmail.com>
2023-08-05 14:29:31 -0700

    Print numbers from 0 to 10

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Prints the alphabet uppercase and lowercase letters
+ * Return: Always (Success)
+ */
+
+int main(void)
+{
+	int n = 0;
+
+	for (n = 0; n < 10; n++)
+	printf("%d", n);
+	printf("\n");
+
+	return (0);
+}
