419745d57eb7ea3d034fe7180ac411e9ef27607f
isaac2036 <isaacpablo0596@hotmail.com>
2023-08-13 00:58:48 -0700

    Feat: Function that adds two integers and returns the result

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - add two integers and returns the result
+ * @n1: first number of the sum
+ * @n2: second number of the sum
+ * Return: integer value
+ */
+
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
