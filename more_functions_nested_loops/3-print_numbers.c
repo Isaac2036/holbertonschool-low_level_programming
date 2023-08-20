b2dba4778b4d26cc4b38908d6da07a2fc8a74aa1
isaac2036 <isaacpablo0596@hotmail.com>
2023-08-20 08:04:00 -0700

    Feat: function that prints the numbers, from 0 to 9, followed by a new line

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two int
+ * @x: The first number to be checked
+ * @y: The second number to be checked
+ * Return: Multiplication of integers
+ */
+
+void print_numbers(void)
+{
+	int a = 0;
+	
+	while(a <= 9)
+	{
+	_putchar(a + '0');
+	a++;
+	}
+
+	_putchar('\n');
+}
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,3 +1,5 @@
+int _putchar(char c);
 int _isupper(int x);
 int _isdigit(int x);
 int mul(int x, int y);
+void print_numbers(void);
