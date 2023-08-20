5a0c3551390a845de2bec5e39b81dc1db578a705
isaac2036 <isaacpablo0596@hotmail.com>
2023-08-20 07:37:33 -0700

    Feat: function that multiplies two integers

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two int
+ * @x: The first number to be checked
+ * @y: The second number to be checked
+ * Return: Multiplication of integers
+ */
+
+int mul(int x, int y)
+{
+	return (x * y);
+}
