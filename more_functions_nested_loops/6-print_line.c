e74da71db725e8fd29f76b85f14997a71cc9f152
isaac2036 <isaacpablo0596@hotmail.com>
2023-08-20 09:36:04 -0700

    Feat: function that draws a straight line in the terminal

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Displays numbers 0 to 9 except 2 and 4
+ * Return: The number from 0 to 9
+ */
+
+void print_line(int n)
+{
+	int a = 0;
+
+	for (; a <= n; a++)
+	{
+	_putchar('_');
+	}
+	_putchar ('$');
+	_putchar ('\n');
+}
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -5,3 +5,4 @@ int mul(int x, int y);
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
+void print_line(int n);
