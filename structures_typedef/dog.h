84bdccf22d0a1ef38585ff57946cf93a31f52ce6
isaac2036 <isaacpablo0596@hotmail.com>
2023-09-09 13:45:40 -0700

    Feat: new type struct dog

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -11,29 +11,20 @@
  *@age: Age of dog.
  *@owner: Owner of dog.
  */
-typedef struct dog
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-} dog;
+};
 
 /**
- * struct dog_t - Structut for created a dog.
- *@name: Name of dog.
- *@age: Age of dog.
- *@owner: Owner of dog.
+ * dog_t - Typedef for dog structure
  */
-typedef struct dog_t
-{
-	char *name;
-	float age;
-	char *owner;
-} dog_t;
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
-
+void free_dog(dog_t *d);
 #endif
